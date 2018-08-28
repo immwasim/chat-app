a9812127b94f333cb5ee36767608ee27608ecc06
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-28 03:47:42 -0400

    added socket io

--- server/server.js
+++ server/server.js
@@ -1,18 +1,27 @@
 require('./../config/config');
 
 var express = require('express');
+var socketIO = require('socket.io');
 const port = process.env.PORT;
 
+const http = require('http'); //built in
 const path = require('path'); //built in
 
 const publicPath = path.join(__dirname, '../public');
-const serverPath = path.join(__dirname, '../server');
 
 var app = express();
+var server = http.createServer(app); //using http server not express
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+        console.log("client connection dropped");
+    })
+})
 
 app.use(express.static(publicPath));
-//routes
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server started on ${port}`);
 });
